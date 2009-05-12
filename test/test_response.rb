c76cb4174081839346290ad7b169506227c0cf38
Ryan Funduk <ryan.funduk@gmail.com>
2009-05-12 16:10:09 -0400

    Response tests

---
+++ test/test_response.rb
@@ -0,0 +1,50 @@
+require 'test/unit'
+require 'redgreen'
+$:.unshift( File.dirname( File.expand_path( __FILE__ ) ) + '/../lib/' )
+require 'comfy'
+
+class TestDocument < Test::Unit::TestCase
+  include Comfy
+
+  def setup
+    @doc = { :ok => true, :weak => 'sauce', :answer => 42 }
+    @response = Response.new( @doc.to_json )
+  end
+
+  def test_has
+    assert_nothing_raised do
+      @response.ensure( :has => 'ok' )
+    end
+
+    assert_raises Comfy::ResponseSanityFail do
+      @response.ensure( :has => 'style' )
+    end
+  end
+
+  def test_lacks
+    assert_nothing_raised do
+      @response.ensure( :lacks => 'style' )
+    end
+
+    assert_raises Comfy::ResponseSanityFail do
+      @response.ensure( :lacks => 'ok' )
+    end
+  end
+
+  def test_to_doc
+    @doc.entries.each do |key, value|
+      assert_equal value, @response.send( key )
+    end
+  end
+
+  def test_name_mangling
+    @doc['id'] = 'laksjdf'
+    @doc['to_s'] = 'not_a_function'
+
+    @response = Response.new( @doc.to_json )
+
+    assert_equal @doc['id'], @response._id
+    assert_equal @doc['to_s'], @response._to_s
+    assert_not_equal @doc['to_s'], @response.to_s
+  end
+end
