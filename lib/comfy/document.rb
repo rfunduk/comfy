9bd397579f56f130892722441f3b55c4a1c6ba28
Ryan Funduk <ryan.funduk@gmail.com>
2009-06-03 15:35:14 -0400

    support bulk_save with hashes _and_ Document's, doing appropriate conversions

--- lib/comfy/document.rb
+++ lib/comfy/document.rb
@@ -2,9 +2,9 @@ module Comfy
 
   class Document
     include Comfy
-    
+
     attr_reader :db
-    
+
     def initialize( hash={}, db=Comfy::Config.db )
       hash = JSON.parse( hash ) if hash.is_a? String
       @__hash = {}
@@ -15,7 +15,13 @@ module Comfy
       end
       @db = db
     end
-    
+
+    def self.create( hash={}, db=Comfy::Config.db )
+      doc = self.new( hash, db )
+      doc.save
+      doc
+    end
+
     def method_missing( method, *args, &block )
       method = method.to_s
       if method =~ /=$/
@@ -41,7 +47,7 @@ module Comfy
       end
       return true
     end
-    
+
     def save
       raise Comfy::UnsaveableDocumentException if @db.nil?
       
@@ -62,22 +68,26 @@ module Comfy
 
     def delete
       raise Comfy::UnsaveableDocumentException if @db.nil?
-
+      
       begin
         result = RCW.delete( "#{@db.uri}/#{_id}?rev=#{_rev}" )
       rescue
         return false
       end
-
+      
       return Response.new( result, @db )
     end
-    
+
     def self.bulk_save( docs, db=Comfy::Config.db )
-      body = { 'docs' => docs.collect { |doc| doc.hash } }
+      docs = docs.collect { |doc| doc.is_a?(Document) ? doc.hash : doc }
+      body = { 'docs' => docs }
       result = RCW.post( db.uri + '/_bulk_docs', body.to_json,
                          :content_type => 'application/json' )
       zipped = JSON.parse( result ).zip( docs )
+      
       updated_docs = zipped.entries.collect do |result, original_doc|
+        original_doc = Document.new( original_doc ) \
+          unless original_doc.is_a? Document
         Response.new( result.to_json ).ensure( :lacks => 'error' )
         original_doc._rev = result['rev']
         original_doc._id = result['id']
@@ -99,14 +109,13 @@ module Comfy
         unless params.empty?
       db.get( uri ).to_doc
     end
-    
+
     protected
 
     def has?( key )
       key = key.to_s if key.is_a? Symbol
       return @__hash.has_key?( key )
     end
-    
   end
 
 end
